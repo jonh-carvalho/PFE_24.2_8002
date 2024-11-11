765b07dd7c08dfeff1b29ad13030d5db492ed33a
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2024-11-11 14:07:24 -0300

    Deployed e9db9bf with MkDocs version: 1.6.1

---
+++ _Disciplina/Code/aula/my-react-app/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import FormLogin from "../components/FormLogin.jsx";
+import Hero from "../components/Hero.jsx";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      
+      <FormLogin />
+      {/*
+      <Features />
+    <Testimonials />);
+    */}
+    
+    </>
+  );
+}
+
+export default Home;
