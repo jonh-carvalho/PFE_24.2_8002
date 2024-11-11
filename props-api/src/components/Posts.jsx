bd1e7c7e96a2e3d43cc9fc252e6b9c855aae5a97
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2024-11-11 13:43:59 -0300

    update aula react

---
+++ props-api/src/components/Posts.jsx
@@ -0,0 +1,15 @@
+function Posts({ posts }) {
+    return (
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  export default Posts;
+  
